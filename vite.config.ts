a5c294cfe1dcb3cf66cba65980b8a398a492cb9b
Ruchi-890 <ruchi94721@gmail.com>
2025-07-05 21:11:48 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: '/category/streak/vision',
+  base: '/vite/react/shadcn/ts',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
